5d9cc495ff8f5ace1d061dd05a03bdf2411ce214
Raihan Titahjurnalis Mimpangyuda <raihanninja@gmail.com>
2021-09-10 16:52:47 +0700

    push

---
+++ PTS/Assets/Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public GameObject gameObject1;
+    public GameObject gameObject2;
+    
+
+    void Start()
+    {
+        if(gameObject1 == true)
+        {
+            gameObject2.transform.Rotate(3, 5, 7);
+            Debug.Log("The object is rotating");
+        }    
+    }
+    void OnDisable()
+    {
+        Debug.Log("The Object is Disable");
+    }
+
+}
